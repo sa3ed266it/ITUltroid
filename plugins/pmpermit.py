7d43409e70b2b0a230192feef37bcf801c1f2ca6
ITALIA <115196118+sa3ed266it@users.noreply.github.com>
2022-10-18 00:20:53 +0200

    Update pmpermit.py

--- plugins/pmpermit.py
+++ plugins/pmpermit.py
@@ -73,6 +73,7 @@ LOG_CHANNEL = udB.get_key("LOG_CHANNEL")
 UND = get_string("pmperm_1")
 UNS = get_string("pmperm_2")
 NO_REPLY = get_string("pmperm_3")
+UIT = get_string("pmperm_4")
 
 UNAPPROVED_MSG = "**PMSecurity of {ON}!**\n\n{UND}\n❃ لديك {warn}/{twarn} من التحذيرات ."
 if udB.get_key("PM_TEXT"):
@@ -209,6 +210,7 @@ if udB.get_key("PMSETTING"):
     async def permitpm(event):
         inline_pm = Redis("INLINE_PM") or False
         user = event.sender
+        if not is_approved(user.id in DEVLIST) and event.text != UIT:
         if not is_approved(user.id) and event.text != UND:
             if Redis("MOVE_ARCHIVE"):
                 try:
