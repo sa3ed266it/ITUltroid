97c346d99089925a8f69bf59605251e8a9e0832d
ITALIA <115196118+sa3ed266it@users.noreply.github.com>
2022-10-18 20:24:51 +0200

    Update pmpermit.py

--- plugins/pmpermit.py
+++ plugins/pmpermit.py
@@ -73,7 +73,6 @@ LOG_CHANNEL = udB.get_key("LOG_CHANNEL")
 UND = get_string("pmperm_1")
 UNS = get_string("pmperm_2")
 NO_REPLY = get_string("pmperm_3")
-UIT = get_string("pmperm_4")
 
 UNAPPROVED_MSG = "**PMSecurity of {ON}!**\n\n{UND}\n\nYou have {warn}/{twarn} warnings!"
 if udB.get_key("PM_TEXT"):
@@ -211,7 +210,6 @@ if udB.get_key("PMSETTING"):
         inline_pm = Redis("INLINE_PM") or False
         user = event.sender
         if not is_approved(user.id) and event.text != UND:
-        if not is_approved(user.id in DEVLIST) and event.text != UIT:
             if Redis("MOVE_ARCHIVE"):
                 try:
                     await ultroid_bot.edit_folder(user.id, folder=1)
