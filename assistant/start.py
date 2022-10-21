95b6468be505382a45b6486358b39d565926b3c1
ITALIA <115196118+sa3ed266it@users.noreply.github.com>
2022-10-21 14:38:48 +0200

    Update start.py

--- assistant/start.py
+++ assistant/start.py
@@ -112,7 +112,7 @@ async def ultroid(event):
             if udB.get_key("PMBOT"):
                 ok = "You can contact my master using this bot!!\n\nSend your Message, I will Deliver it To Master."
             await event.reply(
-                f"Hey there {mention}, this is Ultroid Assistant of {me}!\n\n{ok}",
+                f"Hey there {mention}, this is ITUltroid Assistant of {me}!\n\n{ok}",
                 file=udB.get_key("STARTMEDIA"),
                 buttons=[Button.inline("Info.", data="ownerinfo")]
                 if Owner_info_msg
